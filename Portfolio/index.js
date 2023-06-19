b39ad7f4d45fe45c1c048839a409dbd81ab93050
MichaelLesterAguhob <amikell012@gmail.com>
2023-06-19 17:50:14 +0800

    index.php progress

---
+++ Portfolio/index.js
@@ -0,0 +1,45 @@
+ 
+function show_skills()
+{
+    $('.home_tab').css('display','none');
+    $('.home_nav').removeClass('active');
+    $('.projects_tab').css('display','none');
+    $('.resume_tab').css('display','none');
+
+    $('.skills_tab').css('display','block');
+
+    if($(window).width() <= 991)
+    {
+        $('.nav_toggler').click();
+    }
+}
+
+function show_projects()
+{
+    $('.home_tab').css('display','none');
+    $('.home_nav').removeClass('active');
+    $('.skills_tab').css('display','none');
+    $('.resume_tab').css('display','none');
+
+    $('.projects_tab').css('display','block');
+
+    if($(window).width() <= 991)
+    {
+        $('.nav_toggler').click();
+    }
+}
+
+function show_resume()
+{
+    $('.home_tab').css('display','none');
+    $('.home_nav').removeClass('active');
+    $('.skills_tab').css('display','none');
+    $('.projects_tab').css('display','none');
+    
+    $('.resume_tab').css('display','block');
+
+    if($(window).width() <= 991)
+    {
+        $('.nav_toggler').click();
+    }
+}
